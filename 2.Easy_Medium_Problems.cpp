0f79f87050b8d8f388b397cc7b526b51eb2af23c
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-07-07 11:33:46 +0530

    Update 2.Easy_Medium_Problems.cpp

--- 2.Easy_Medium_Problems.cpp
+++ 2.Easy_Medium_Problems.cpp
@@ -62,3 +62,24 @@
          return {g2,g1};
     }
 
+
+//POWER of x^n
+// here x always +ve considered
+double myPow(double x, int n) {
+  double ans = 1.0;
+  long long nn = n;
+  if (nn < 0) nn = -1 * nn;
+  while (nn>0) {
+    if (nn % 2==0) {
+      x = x * x;
+      nn = nn / 2;
+     
+    } else {
+      ans = ans * x;
+      nn = nn - 1;
+    }
+  }
+  if (n < 0) ans = (double)(1.0) / (double)(ans);
+  return ans;
+}
+

0fc0b23ff89fc78152043051e79f5c3c801a6d21
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-09-21 22:29:25 +0530

    Update 2.Easy_Medium_Problems.cpp

--- 2.Easy_Medium_Problems.cpp
+++ 2.Easy_Medium_Problems.cpp
@@ -1,3 +1,14 @@
+//Reverse bits of a given integer
+unsigned int reverseBits(unsigned int n) {
+unsigned int result=0;
+for(int i=0;i<32;i++){
+	result<<=1;
+	result|=(n&1);
+	n>>=1;
+}
+return result;
+}
+
 // Number of bit to be flipped to convert A --> B
     int minBitFlips(int start, int goal) {
         int xr=start^goal;

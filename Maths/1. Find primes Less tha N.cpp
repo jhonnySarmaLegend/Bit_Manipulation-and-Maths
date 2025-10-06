87c60692b14fec9fd70f3e35f8fc1983f89cd271
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-10-06 16:19:44 +0530

    Update 1. Find primes Less tha N.cpp

--- Maths/1. Find primes Less tha N.cpp
+++ Maths/1. Find primes Less tha N.cpp
@@ -25,7 +25,7 @@ class Solution {
 public:
     int countPrimes(int n) {
         vector<int>p(n,1);
-        for(int i=2;i<=sqrt(n);i++){
+        for(int i=2;i*i<=n;i++){
             if(p[i]==1){
                 for(int j=i*i;j<n;j+=i){
                     p[j]=0;
